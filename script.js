fe1d82c145f9b471b0f0712e3e42366ddf5806e2
Edwin Chen <edwinchen85@gmail.com>
2017-05-25 21:31:20 +0800

    Define setTopPadding function to find header height and set as main padding top

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 var m = document.querySelector("main"),
     h = document.querySelector("header"),
     hHeight;
+
+function setTopPadding() {
+  hHeight = h.offsetHeight;
+  m.style.paddingTop = hHeight + "px";
+}
