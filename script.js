753192b75a91ea1eebee33a51e1e8b3af34658e0
Edwin Chen <edwinchen85@gmail.com>
2017-05-25 21:35:58 +0800

    Define onScroll func. Add scroll event listener and execute callback to add or remove 'scroll' class when page scroll 150 from top.

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ function setTopPadding() {
   hHeight = h.offsetHeight;
   m.style.paddingTop = hHeight + "px";
 }
+
+function onScroll() {
+  window.addEventListener("scroll", callbackFunc);
+  function callbackFunc() {
+    var y = window.pageYOffset;
+    if (y > 150) {
+      h.classList.add("scroll");
+    } else {
+      h.classList.remove("scroll");
+    }
+  }
+}
